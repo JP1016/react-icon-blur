701b7728604dc4547387d0fa473ee2ad575509bf
Jithin Pariyarath <emast007@gmail.com>
2021-05-01 01:16:55 +0530

    edited icon width

--- src/index.tsx
+++ src/index.tsx
@@ -18,15 +18,13 @@ const IconBlur = (props: IconProps) => (
     <div className={`icon-container ${props.onClick ? 'pointer' : ''}`}>
       <div
         className={`icon ${props.type.toLowerCase()}`}
-        style={{ padding: props.padding ? `${props.padding}px` : '0px' }}
+        style={{
+          padding: props.padding ? `${props.padding}px` : '5px',
+          width: props.size ? `${props.size}px` : '100px',
+        }}
       >
         <img alt="" aria-hidden src={props.src} className="background" />
-        <img
-          alt={`icon-${props.name}`}
-          src={props.src}
-          width={props.size}
-          height={props.size}
-        />
+        <img alt={`icon-${props.name}`} src={props.src} width="100%" />
       </div>
     </div>
   </div>
